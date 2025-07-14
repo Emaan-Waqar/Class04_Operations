7b08639498e0fc39568ace947491a7cef94de0ba
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-14 13:52:56 +0500

    Average

---
+++ Average.py
@@ -0,0 +1,14 @@
+#storing values
+tree_1= 98
+tree_2= 36
+tree_3= 52
+tree_4= 83
+tree_5= 76
+
+#Sum of the height of the trees
+sum= tree_1+ tree_2+ tree_3+ tree_4+ tree_5
+print("The sum of the trees' height is:", sum)
+
+#Finding the average of the trees' height
+Average= sum/5
+print("The average height of trees is:", Average)
